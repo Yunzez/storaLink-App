7c0bb36ce023257077f7e91aadef162779cfa866
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2023-12-19 21:05:21 -0500

    add a few routes

---
+++ storalink-app/storalink-app/Views/SettingViews/UserInfoSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  UserInfoSettingView.swift
+//  storalink-app
+//
+//  Created by Yunze Zhao on 12/19/23.
+//
+
+import SwiftUI
+
+struct UserInfoSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserInfoSettingView()
+}
